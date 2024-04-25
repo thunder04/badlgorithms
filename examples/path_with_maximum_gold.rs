4ed9f7d6ae0237ce673294224749b7c41c2c5c22
thunder04 <70254851+thunder04@users.noreply.github.com>
2024-04-25 23:01:37 +0300

    Convert the recursive function to top-down

--- examples/path_with_maximum_gold.rs
+++ examples/path_with_maximum_gold.rs
@@ -13,12 +13,12 @@
 /// ## Solution
 ///
 /// Provided as an assignment from my professor in Algorithms, this is my solution.
-/// It is solved as a dynamic programming (bottom-up version) problem:
+/// It is solved as a dynamic programming (top-down version) problem:
 ///
 /// ```
 /// r(x, y) = {
-///   0, if x = 0 or y = 0,
-///   f(x, y) + max{ r(x - 1, y), r(x, y - 1), r(x - 1, y - 1) }, elsewhere
+///   0, if x = N or y = N,
+///   f(x, y) + max{ r(x + 1, y), r(x, y + 1), r(x + 1, y + 1) }, elsewhere
 /// }
 /// ```
 ///
@@ -64,17 +64,16 @@ fn main() {
 
 type Num = u16;
 type Grid<const N: usize> = [[Num; N]; N];
-type TabulationTable<const N: usize> = [[Option<(Num, Direction)>; N]; N];
-type GoldPositionCanditate = (Cell<isize>, Direction);
+type MemoizationTable<const N: usize> = [[Option<(Num, Direction)>; N]; N];
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-struct Cell<T> {
-    pub x: T,
-    pub y: T,
+struct Cell {
+    pub x: usize,
+    pub y: usize,
 }
 
-impl<T> From<(T, T)> for Cell<T> {
-    fn from(value: (T, T)) -> Self {
+impl From<(usize, usize)> for Cell {
+    fn from(value: (usize, usize)) -> Self {
         Self {
             x: value.0,
             y: value.1,
@@ -84,26 +83,26 @@ impl<T> From<(T, T)> for Cell<T> {
 
 #[derive(Clone, Copy, Debug)]
 enum Direction {
-    Left,
-    Top,
-    DiagonalTL,
+    Right,
+    Bottom,
+    DiagonalBR,
 }
 
-fn path_with_maximum_gold<const N: usize>(grid: &Grid<N>) -> (Num, Vec<Cell<usize>>) {
-    let ending_cell: Cell<isize> = ((N - 1) as _, (N - 1) as _).into();
+fn path_with_maximum_gold<const N: usize>(grid: &Grid<N>) -> (Num, Vec<Cell>) {
+    let starting_cell = Cell { x: 0, y: 0 };
 
-    let mut tabulation_table: TabulationTable<N> = [[None; N]; N];
-    let max_amount = path_with_maximum_gold_sol(grid, &mut tabulation_table, ending_cell);
+    let mut memoization_table: MemoizationTable<N> = [[None; N]; N];
+    let max_amount = path_with_maximum_gold_sol(grid, &mut memoization_table, starting_cell);
 
     // Find the path via the tabulation table
-    let mut path: Vec<Cell<usize>> = [((N - 1) as _, (N - 1) as _).into()].to_vec();
-    let mut curr_cell = path[0];
+    let mut path: Vec<Cell> = [starting_cell].to_vec();
+    let mut curr_cell = starting_cell;
 
-    while curr_cell != (Cell { x: 0, y: 0 }) {
-        curr_cell = match tabulation_table[curr_cell.y][curr_cell.x] {
-            Some((_, Direction::Left)) => (curr_cell.x - 1, curr_cell.y).into(),
-            Some((_, Direction::Top)) => (curr_cell.x, curr_cell.y - 1).into(),
-            Some((_, Direction::DiagonalTL)) => (curr_cell.x - 1, curr_cell.y - 1).into(),
+    while curr_cell != (Cell { x: N - 1, y: N - 1 }) {
+        curr_cell = match memoization_table[curr_cell.y][curr_cell.x] {
+            Some((_, Direction::Right)) => (curr_cell.x + 1, curr_cell.y).into(),
+            Some((_, Direction::Bottom)) => (curr_cell.x, curr_cell.y + 1).into(),
+            Some((_, Direction::DiagonalBR)) => (curr_cell.x + 1, curr_cell.y + 1).into(),
             None => continue,
         };
 
@@ -115,33 +114,27 @@ fn path_with_maximum_gold<const N: usize>(grid: &Grid<N>) -> (Num, Vec<Cell<usiz
 
 fn path_with_maximum_gold_sol<const N: usize>(
     grid: &Grid<N>,
-    tabulation_table: &mut TabulationTable<N>,
-    cell: Cell<isize>,
+    memoization_table: &mut MemoizationTable<N>,
+    cell: Cell,
 ) -> Num {
     // If x or y are out of bounds
-    if cell.x < 0 || cell.y < 0 {
+    if cell.x == N || cell.y == N {
         return 0;
     }
 
-    let (cell_x, cell_y) = (cell.x as usize, cell.y as usize);
-
     // Return cached value if it exists
-    if let Some((cached, _)) = tabulation_table[cell_y][cell_x] {
+    if let Some((cached, _)) = memoization_table[cell.y][cell.x] {
         return cached;
     }
 
-    let (mut max_dir, mut max_gold) = (Direction::Left, u16::MIN);
+    let diagonal_br_gold = (Cell::from((cell.x + 1, cell.y + 1)), Direction::DiagonalBR);
+    let bottom_gold = (Cell::from((cell.x, cell.y + 1)), Direction::Bottom);
+    let right_gold = (Cell::from((cell.x + 1, cell.y)), Direction::Right);
 
-    // Solving it bottom-up also means to mirror diagonally the moving constraint,
-    // so go left, up and diagonal up-left.
+    let (mut max_dir, mut max_gold) = (Direction::Bottom, u16::MIN);
 
-    let top_gold: GoldPositionCanditate = ((cell.x, cell.y - 1).into(), Direction::Top);
-    let left_gold: GoldPositionCanditate = ((cell.x - 1, cell.y).into(), Direction::Left);
-    let diagonal_tl_gold: GoldPositionCanditate =
-        ((cell.x - 1, cell.y - 1).into(), Direction::DiagonalTL);
-
-    for (cell, direction) in [left_gold, top_gold, diagonal_tl_gold] {
-        let gold = path_with_maximum_gold_sol(grid, tabulation_table, cell);
+    for (cell, direction) in [right_gold, bottom_gold, diagonal_br_gold] {
+        let gold = path_with_maximum_gold_sol(grid, memoization_table, cell);
 
         if gold > max_gold {
             max_dir = direction;
@@ -149,9 +142,9 @@ fn path_with_maximum_gold_sol<const N: usize>(
         }
     }
 
-    let amount = grid[cell_y][cell_x] + max_gold;
+    let amount = grid[cell.y][cell.x] + max_gold;
 
     // Cache and return the result
-    tabulation_table[cell_y][cell_x] = Some((amount, max_dir));
+    memoization_table[cell.y][cell.x] = Some((amount, max_dir));
     amount
 }
